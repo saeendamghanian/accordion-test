c95b6ffe4547748a304765ff0a9e83541b682a2d
Saeen-Damghanian <saeen.damghanian@gmail.com>
2020-02-27 20:06:51 +0100

    Change content

--- JS/accordion.js
+++ JS/accordion.js
@@ -5,20 +5,20 @@ var options = {
     {
       title: "My profile",
       description:
-        "Tempor exercitation do dolor elit. Cillum excepteur duis aliqua fugiat veniam exercitation elit aliqua ipsum tempor cupidatat. Nisi nulla dolore sunt enim pariatur do tempor elit aliquip aute proident laborum.",
+        "Change your profile information, email and other personal detailes. Tempor exercitation do dolor elit. Cillum excepteur duis aliqua fugiat veniam exercitation elit aliqua ipsum tempor cupidatat. Nisi nulla dolore sunt enim pariatur do tempor elit aliquip aute proident laborum.",
       content:
         "<p>Dolor enim deserunt anim proident sint ex aliqua qui. Do nostrud officia ipsum deserunt cillum. Veniam deserunt adipisicing consequat commodo consequat commodo magna. Non cillum enim elit occaecat velit incididunt duis eiusmod sint aliqua sunt ullamco proident occaecat. Esse officia pariatur Lorem non quis amet do exercitation cupidatat adipisicing et eu ipsum.</p>"
     },
     {
       title: "Change password",
       description:
-        "Deserunt qui do ipsum id deserunt deserunt consectetur incididunt. Enim cillum do culpa sunt dolore mollit ex aute quis duis enim Lorem. Officia do anim et Lorem eiusmod ut in ut reprehenderit Lorem aliquip laborum nulla ullamco. Sunt pariatur quis adipisicing non consequat eiusmod ut officia exercitation ea dolore irure fugiat reprehenderit. Occaecat irure qui dolor velit.",
+        "Change your password to access the LMS. Deserunt qui do ipsum id deserunt deserunt consectetur incididunt. Enim cillum do culpa sunt dolore mollit ex aute quis duis enim Lorem. Officia do anim et Lorem eiusmod ut in ut reprehenderit Lorem aliquip laborum nulla ullamco. Sunt pariatur quis adipisicing non consequat eiusmod ut officia exercitation ea dolore irure fugiat reprehenderit. Occaecat irure qui dolor velit.",
       content:
         "<p>Lorem Lorem dolore magna id irure aliquip tempor deserunt sunt fugiat non. Velit nostrud nostrud exercitation magna Lorem tempor eiusmod enim nulla. Reprehenderit laborum qui occaecat quis sint dolor proident quis ea ad. Sunt ullamco proident tempor mollit fugiat ex culpa excepteur cillum officia do consequat voluptate id. Deserunt id nostrud occaecat exercitation tempor quis commodo aliquip voluptate consequat ex proident ad aliquip. Sit et anim ullamco dolor pariatur nostrud ex labore est aliquip officia cillum.</p>"
     },
     {
-      title: "Change password",
-      description: "Change your login password",
+      title: "Preferences",
+      description: "Set your language and timezone preferences.",
       content:
         "<p>Cupidatat eu reprehenderit consequat non proident nostrud id voluptate veniam eiusmod cupidatat. Minim irure dolore cupidatat ullamco. Dolore do elit duis et quis tempor laborum occaecat laborum ea mollit. Pariatur consectetur aliquip veniam nostrud culpa aliqua exercitation consequat non.</p>"
     },
